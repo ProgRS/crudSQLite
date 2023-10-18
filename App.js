2b635e83bbf821f1063fb83d18c69b97fc6c60af
ProgRs <lfernandotc@hotmail.com>
2023-10-18 04:18:39 -0300

    Implementação APP utilizando SQLite
    Implementação APP utilizando SQLite

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StatusBar, StyleSheet } from "react-native"
+import NotaEditor from "./src/componentes/NotaEditor"
+import { Nota } from "./src/componentes/Nota"
+import { useEffect, useState } from "react"
+import { buscaNotas, criaTabela } from "./src/servicos/Notas"
 
 export default function App() {
+
+  useEffect(()=>{
+    criaTabela()
+    mostraNotas()
+  },[])
+
+  const [notas, setNotas] = useState([])
+
+  async function mostraNotas() {
+    const todasNotas = await buscaNotas()
+    setNotas(todasNotas)
+    console.log(todasNotas)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={estilos.container}>
+      <FlatList
+        data={notas}
+        renderItem={(nota) => <Nota {...nota}/>}
+        keyExtractor={nota => nota.id}/>
+      <NotaEditor mostraNotas={mostraNotas}/>
+      <StatusBar/>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const estilos = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "stretch",
+		justifyContent: "flex-start",
+	},
+})
+
---
+++ src/componentes/Nota.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export function Nota({item}) {
+  const categorias = {Pessoal: "#FF924F", Outros: "#00911F", Trabalho: "#2F71EB"}
+  const estilos = styleFunction(categorias[item.categoria])
+  
+  return (
+    <View style={estilos.cartao}>
+      <Text style={estilos.titulo}>{item.titulo}</Text>
+      <Text style={estilos.categoria}>{item.categoria}</Text>
+      <Text style={estilos.texto} numberOfLines={5}>{item.texto}</Text>
+    </View>
+  )
+}
+
+const styleFunction = (cor) => StyleSheet.create({
+  cartao: {
+    borderRadius: 8,
+    backgroundColor: "#ffffff",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    marginHorizontal: 16,
+    marginBottom: 8,
+    borderTopWidth: 5,
+    borderColor: cor,
+    boxShadowColor: "#000",
+    boxShadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    boxShadowOpacity: 0.23,
+    boxShadowRadius: 2.62,
+    elevation: 4,
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: "700",
+    marginBottom: 4,
+  },
+  categoria: {
+    borderRadius: 4,
+    backgroundColor: cor,
+    padding: 4,
+    color: "#FAFAFA",
+    alignSelf: "flex-start",
+  },
+  texto: {
+    lineHeight: 28,
+  }
+})
